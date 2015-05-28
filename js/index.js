9624d20e6d6ed92f54f1697d378d102dce63d34f
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-05-28 11:52:10 -0700

    bug testing AJAX

--- js/index.js
+++ js/index.js
@@ -2,68 +2,21 @@
  * Created by ken on 5/26/15.
  */
 $(function() {
-    var state = true;
-    $( "#button" ).click(function() {
-        if ( state ) {
-            $( "#effect").animate({
-                backgroundColor: "#fff",
-                color: "#000",
-                height: '100%'
-            }, 1000).toggle(function(){
-                $(this).children().show();
-            });
-        } else {
-            $( "#effect" ).animate({
-                backgroundColor: "#fff",
-                color: "#000",
-                height: 0
-            }, 1000).toggle(function(){
-                $(this).children().hide();
-            });
-        }
-        state = !state;
-    });
+    $('#effect').hide();
+    $('#button').on('click', function(){
+        $('#effect').slideToggle(1000);
+    })
 });
 
-//example of posting JSON data to page. Do not use for final code
-var tours = {
-    "whiskey": [
-        {
-            "name": "Hibiki",
-            "location": "Osaka, Japan",
-            "cost": "$80",
-            "type": "Single-malt",
-            "flavor": "Smooth",
-            "tour": "Available"
-        },
-        {
-            "name": "Jack Dainel's",
-            "location": "Tennessee, USA",
-            "cost": "$25",
-            "type": "Bourbon",
-            "flavor": "Sweet",
-            "tour": "Available"
-        }
-    ]
-};
-
-$.each(tours.whiskey, function(i, whiskey){
-    var div = document.createElement('div');
-    var h2 = document.createElement('h2');
-    var p = document.createElement('p');
-    var title = document.createTextNode(whiskey.name);
-    var loc = document.createTextNode(whiskey.location);
-    var cost = document.createTextNode(whiskey.cost);
-    var type = document.createTextNode(whiskey.type);
-    var flavor = document.createTextNode(whiskey.flavor);
-    var tour = document.createTextNode(whiskey.tour);
-
-    p.appendChild(loc);
-    p.appendChild(cost);
-    p.appendChild(type);
-    p.appendChild(flavor);
-    p.appendChild(tour);
-    h2.appendChild(title);
-    div.appendChild(h2);
-    div.appendChild(p);
+var db = $.ajax({
+    url: "https://api.mongolab.com/api/1/databases/whiskey/collections/tuesday",
+    data: {
+        apiKey: "LVnHGETiXJi3beH77dTNrrgbN54PPtB1",
+        q: "{name: {$exists: true}}.sort({name: 1})"
+    }
 });
+
+//$.each(db.responseJSON, function(tour){
+//    console.log(tour.name);
+//});
+console.log(db);
