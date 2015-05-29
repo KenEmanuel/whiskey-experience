5bd4afd47d9dab49097924415b18085f4da71cf4
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-05-29 11:44:46 -0700

    Working on AJAX

--- js/index.js
+++ js/index.js
@@ -8,19 +8,47 @@ $(function() {
     })
 });
 
+var IMAGE_PATH = 'images/';
+
+var listTabs = function(data) {
+    $.each(data, function (i, data) {
+        var $distDiv = $(document.createElement('div'));
+        $distDiv.addClass('col-md-7');
+        var $distA = $(document.createElement('a'));
+        $distA.attr('href', '#');
+        var $distImg = $(document.createElement('img'));
+        $distImg.addClass('img-responsive').attr('src', IMAGE_PATH + data["distillery-image"]);
+        var $descriptionDiv = $(document.createElement('div'));
+        $descriptionDiv.addClass('col-md-5');
+        var $whiskeyH3 = $(document.createElement('h3'));
+        var $priceH4 = $(document.createElement('h4'));
+        var $typeH4 = $(document.createElement('h4'));
+        var $descriptionP = $(document.createElement('p'));
+
+        $whiskeyH3.text(data.whiskey);
+        $priceH4.text(data.price);
+        $typeH4.text(data.type);
+        $descriptionP.text(data.description);
+        //appending image to a tag, then appending a to div, then prepending new div with a and image to div row
+        $distA.append($distImg);
+        $distDiv.append($distA);
+        $('#appendHere').append($distDiv);
+        //appending description. append from first element
+        $descriptionDiv.append($whiskeyH3);
+        $descriptionDiv.append($typeH4);
+        $descriptionDiv.append($priceH4);
+        $descriptionDiv.append($descriptionP);
+        $('#appendHere').append($descriptionDiv);
+    })
+};
+
 $.ajax({
-    url: "https://api.mongolab.com/api/1/databases/whiskey/collections/tuesday",
+    url: "https://api.mongolab.com/api/1/databases/whiskey/collections/whiskies",
     data: {
         apiKey: "LVnHGETiXJi3beH77dTNrrgbN54PPtB1",
-        q: "{name: 'Glinlevit'})"
+        q: "{whiskey: {$exists: true}}"
     },
-    success: function(data) {
-        $.each(data, function (i, data) {
-            $('h3').text(data.name);
-            $('#price').text(data.cost);
-            $('#type').text(data.type);
-        })
-    }
+    success: listTabs(data)
 });
 
 
